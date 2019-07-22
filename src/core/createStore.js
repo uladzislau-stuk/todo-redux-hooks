d153b98be79058431a9935f2668bf97da4d0eb5c
Uladzislau Stuk <uladzislau.stuk@leverx.com>
2019-07-22 14:14:26 +0300

    Connect Redux extension

--- src/core/createStore.js
+++ src/core/createStore.js
@@ -3,15 +3,17 @@ import { combinedReducers } from './combineReducers'
 import { loadState, saveState } from './localStorage'
 import throttle from 'lodash.throttle'
 
-const persistedState = loadState();
+const persistedState = loadState()
+const reduxExtension = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(combinedReducers, persistedState)
+
+const store = createStore(combinedReducers, persistedState, reduxExtension)
 
 store.subscribe(throttle(() => {
 	saveState({
 		filter: store.getState().filter,
-		todos: store.getState().todos
-	});
+		todos : store.getState().todos
+	})
 }, 1000))
 
 export default store
