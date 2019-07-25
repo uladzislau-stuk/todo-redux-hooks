97b2f66b9493f1802f9d2bb70cff8af1ff1305e8
Uladzislau Stuk <uladzislau.stuk@gmail.com>
2019-07-25 16:41:00 +0300

    Add redux devtools extension

--- src/core/createStore.js
+++ src/core/createStore.js
@@ -1,11 +1,16 @@
 import { createStore } from 'redux'
 import { combinedReducers } from './combineReducers'
 import { loadState, saveState } from './localStorage'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 import throttle from 'lodash.throttle'
 
 const persistedState = loadState();
 
-const store = createStore(combinedReducers, persistedState)
+const store = createStore(
+	combinedReducers,
+	persistedState,
+	devToolsEnhancer()
+)
 
 store.subscribe(throttle(() => {
 	saveState({
